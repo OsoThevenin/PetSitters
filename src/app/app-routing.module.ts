765e4ce010d3db0ef732995641ecbb8ac2e9dc19
Pere <perebvilalta@gmail.com>
2019-03-18 12:02:09 +0100

    Implement tutorial slides

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
+import { TutorialGuard } from './guards/tutorial.guard';
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  {
+    path: '',
+    loadChildren: './tabs/tabs.module#TabsPageModule',
+    canActivate: [TutorialGuard]
+  },
+  {
+    path: 'tutorial',
+    loadChildren: './intro/intro.module#IntroPageModule'
+  }
 ];
 @NgModule({
   imports: [
---
+++ src/app/guards/tutorial.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router, CanActivate } from '@angular/router';
+import { Storage } from '@ionic/storage';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class TutorialGuard implements CanActivate {
+  constructor(private storage: Storage, private router: Router) {}
+
+  async canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Promise<boolean> {
+
+    const isComplete = await this.storage.get('tutorialComplete');
+
+    if (!isComplete) {
+      this.router.navigateByUrl('/tutorial');
+    }
+
+    return isComplete;
+  }
+}
