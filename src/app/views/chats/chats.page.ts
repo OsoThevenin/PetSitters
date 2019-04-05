67fbca14459df3637db57d63a3df0f4dbd1a270d
Daniel <danielesquina@gmail.com>
2019-04-05 15:54:24 +0200

    mockup chat feo

--- src/app/views/chats/chats.page.ts
+++ src/app/views/chats/chats.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { stringify } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-chats',
@@ -6,16 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['chats.page.scss']
 })
 export class ChatsPage {
+  data = [
+    {avatar:'../../../assets/default_avatar.png', mensaje:'hey ther as,akefei fefwiefibh ew efi wiebhfihwih ef we fiwfi wi fw i e11111', nombre:'user1'},
+    {avatar:'../../../assets/default_avatar.png', mensaje:'hey there2222', nombre:'user2'},
+    {avatar:'../../../assets/default_avatar.png', mensaje:'hey ther333e', nombre:'user3'},
+    {avatar:'../../../assets/default_avatar.png', mensaje:'hey ther444e', nombre:'user4'},
+];
 
-  chatsUsuario=this.devuelveChatsUsuario()
+  chatsUsuario=this.data;
+
+ 
 
   devuelveChatsUsuario(): any{
-    console.log("hola2")
-    return 
+    
+  return this.data;
   }
 
   abreChat(){
     console.log("hola")
-
   }
 }
