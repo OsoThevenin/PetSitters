d4dafb58a262524ce183af33ca43770baf06e25a
Daniel <danielesquina@gmail.com>
2019-04-10 16:43:35 +0200

    Validators añadidos

--- src/app/views/profile/modal/modal.page.ts
+++ src/app/views/profile/modal/modal.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { FormControl, FormGroup, FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-modal',
@@ -8,9 +9,50 @@ import { ModalController } from '@ionic/angular';
 })
 export class ModalPage implements OnInit {
 
-  constructor(private modalController: ModalController) { }
+  changePasswordForm: FormGroup;
 
+  error_messages ={
+    'password':[
+      {type:'required', message: 'Password is required.'},
+      {type:'minlength', message: 'Password length must be longer or equal than 6 characters.'},
+      {type:'maxlength', message: 'Password must be shorter than 25 characters.'},
+      {type:'pattern', message: 'Password must contain numbers, uppercase and lowercase characters.'}
+    ],
+  };
+
+  @ViewChild('current') current;
+  @ViewChild('new') new;
+  @ViewChild('confirmed') confirmed;
+  
+
+  constructor(private modalController: ModalController,public formBuilder: FormBuilder) { 
+
+    this.changePasswordForm = this.formBuilder.group({
+      pw1fc: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(25),
+        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')
+      ])),
+      pw2fc: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(25),
+        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')
+      ]))
+    });
+
+  }
+
+  
   ngOnInit() {
+  }
+
+  Confirm(){
+    const body: any = {
+      newPassword: this.new.value,
+      oldPassword: this.current.value
+    };
   }
 
   Cancel() {
