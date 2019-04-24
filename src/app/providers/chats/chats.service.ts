4dcbefc4be701ff88300b4e9cb7530920f9929b1
Pere <perebvilalta@gmail.com>
2019-04-24 16:56:14 +0200

    Adds StartChat

---
+++ src/app/providers/chats/chats.service.ts
@@ -0,0 +1,22 @@
+import { GlobalService } from './../../shared/global.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatsService {
+
+  constructor(private http: HttpClient, private global: GlobalService) { }
+
+  startChat(body, token): Observable <any> {
+    let httpHeaders = new HttpHeaders().set('Content-Type', 'application/json');
+    httpHeaders = httpHeaders.append('Access-Control-Allow-Origin', '*');
+    httpHeaders = httpHeaders.append('Authorization', 'Bearer ' + token);
+    const options = {headers: httpHeaders};
+
+    return this.http.post<any>(this.global.baseUrl + 'login', body, options);
+  }
+
+}
--- src/app/views/profile/profile.page.ts
+++ src/app/views/profile/profile.page.ts
@@ -1,3 +1,6 @@
+import { AuthProviderService } from 'src/app/providers/auth/auth-provider.service';
+import { Router } from '@angular/router';
+import { ChatsService } from './../../providers/chats/chats.service';
 import { Component } from '@angular/core';
 import { PopoverController } from '@ionic/angular';
 import { PopoverPage } from './popover/popover.page';
@@ -9,9 +12,24 @@ import { PopoverPage } from './popover/popover.page';
 })
 export class ProfilePage {
 
-  constructor(private popoverCtrl: PopoverController) {
+  constructor(private popoverCtrl: PopoverController, private chatsService: ChatsService,
+    private router: Router, private auth: AuthProviderService) {
   }
 
+  startChat() {
+    this.auth.getToken().then(result => {
+      let body: any = {
+        otherUsername: this.cuidador.username
+      };
+
+      this.chatsService.startChat(body, result)
+      .subscribe(res => {
+        this.router.navigateByUrl('chat');
+      }, err => {
+        console.log('Error al abrir chat');
+      });
+    });
+  }
   async OpenPopover(ev: Event) {
     const popover = await this.popoverCtrl.create({
       component: PopoverPage,
