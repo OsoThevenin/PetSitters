88e75130db427b66f71f66c2cfce6b400406cf8a
Daniel <danielesquina@gmail.com>
2019-05-15 00:09:58 +0200

    acepta y declina contratos, backend no responde bien

--- src/app/providers/auth/auth-provider.service.ts
+++ src/app/providers/auth/auth-provider.service.ts
@@ -102,7 +102,17 @@ export class AuthProviderService {
     httpHeaders = httpHeaders.append('Authorization', 'Bearer ' + token);
     const options = {headers: httpHeaders};
 
-    return this.http.post<any>(this.global.baseUrl + 'acceptContract', data, options);
+    return this.http.post<any>(this.global.baseUrl +'acceptContract?contract='+ data , "", options);
+  }
+
+  rejectContract(data, token): Observable<any> {
+    // Add token to headers
+    let httpHeaders = new HttpHeaders().set('Content-Type', 'application/json');
+    httpHeaders = httpHeaders.append('Access-Control-Allow-Origin', '*');
+    httpHeaders = httpHeaders.append('Authorization', 'Bearer ' + token);
+    const options = {headers: httpHeaders};
+
+    return this.http.delete<any>(this.global.baseUrl + 'acceptContract?contract='+ data, options);
   }
 
 }
--- src/app/views/notification/notification.page.ts
+++ src/app/views/notification/notification.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthProviderService } from 'src/app/providers/auth/auth-provider.service';
 import { throwError } from 'rxjs/internal/observable/throwError';
 import { ToastController } from '@ionic/angular';
@@ -8,11 +8,15 @@ import { ToastController } from '@ionic/angular';
   templateUrl: 'notification.page.html',
   styleUrls: ['notification.page.scss']
 })
-export class NotificationPage {
-
-  x = this.getContracts();
+export class NotificationPage implements OnInit  {
+  
+  contracts: any;
 
   constructor( private toastCtrl: ToastController, private auth: AuthProviderService) {
+  }
+
+  ngOnInit() {
+    this.getContracts();
   }
 
   getContracts() {
@@ -21,20 +25,20 @@ export class NotificationPage {
 
      this.auth.getReceivedContracts(token).subscribe(res => {
        console.log(res);
+       this.contracts=res;
       });
     }).catch(err => {
       console.log(err);
      return throwError;
     });
-    return "pruebaDANIEL!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
   }
-
-  accept(){
+ 
+  accept(un:string){
     this.auth.getToken().then(result => {
-      const data: any = {
-        //username: username
+      const data: any = {///daniel:borra esto si no lo usas
+        contract: un
       };
-      this.auth.acceptContract(data, result)
+      this.auth.acceptContract(un, result)
       .subscribe(res => {
         this.presentToast('You have accepted the contract successfully!');
       }, err => {
@@ -42,6 +46,22 @@ export class NotificationPage {
         console.log(err);
       });
     });
+    this.ngOnInit();
+  }
+  decline(un:string){
+    this.auth.getToken().then(result => {
+      const data: any = {///daniel:borra esto si no lo usas
+        contract: un
+      };
+      this.auth.acceptContract(un, result)
+      .subscribe(res => {
+        this.presentToast('You have rejected the contract successfully!');
+      }, err => {
+        this.presentToast('Something went wrong, please try again');
+        console.log(err);
+      });
+    });
+    this.ngOnInit();
   }
 
   async presentToast(message) {
