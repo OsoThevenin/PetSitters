163405ee5e6fdf3b90b881ec3f8d3355c5858889
Deivid <dglandero@gmail.com>
2019-06-02 18:27:23 +0200

    Show previous valuations on profile

---
+++ src/app/providers/profile/profile.service.ts
@@ -0,0 +1,20 @@
+import { GlobalService } from '../../shared/global.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+
+  constructor(private http: HttpClient, private global: GlobalService) { }
+
+getPreviousValuations(token): Observable <any> {
+    let httpHeaders = new HttpHeaders().set('Content-Type', 'application/json');
+    httpHeaders = httpHeaders.append('Access-Control-Allow-Origin', '*');
+    httpHeaders = httpHeaders.append('Authorization', 'Bearer ' + token);
+    const options = {headers: httpHeaders};
+    return this.http.get<any>(this.global.baseUrl + 'getValuations', options);
+}
+}
