5c95bcdc9b2be741c16d9e66271c572572c43b35
Deivid <dglandero@gmail.com>
2019-06-07 00:33:10 +0200

    Profile pic ranking

--- src/app/views/ranking/ranking.page.ts
+++ src/app/views/ranking/ranking.page.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { AuthProviderService } from 'src/app/providers/auth/auth-provider.service';
 import { throwError } from 'rxjs';
+import { ImageService } from 'src/app/services/image/image.service';
+import { WebView } from '@ionic-native/ionic-webview/ngx';
 
 @Component({
   selector: 'app-ranking',
@@ -11,7 +13,7 @@ export class RankingPage implements OnInit {
 
   ranking = []
 
-  constructor(private auth: AuthProviderService) { }
+  constructor(private auth: AuthProviderService, private imageService: ImageService, private webview: WebView, private ref: ChangeDetectorRef) { }
 
   doRefresh(event) {
     console.log('Begin async operation');
@@ -25,6 +27,7 @@ export class RankingPage implements OnInit {
 
   ngOnInit() {
     this.ranking = this.getRanking();
+    this.downloadProfileImages();
   }
   getRanking(): any {
     this.auth.getToken().then(result => {
@@ -39,5 +42,29 @@ export class RankingPage implements OnInit {
 	    return throwError;
     });
     return this.ranking;
+  }
+
+  downloadProfileImages() {
+    this.imageService.getToken().then((token) => {
+      console.log('cuidadors: ' + JSON.stringify(this.ranking));
+      for (let profile of this.ranking) {
+        if(profile.profileImage !== null) {
+          this.imageService.getImageData(profile.profileImage, token)
+              .then((response) => {
+                console.log('Imatge descarregada: ' + JSON.stringify(response));
+                //let dataDirectory = this.file.externalApplicationStorageDirectory;
+                //let url = dataDirectory + '/files/received/' + filename + '.jpg';
+  
+                let imagePath = this.webview.convertFileSrc(response.nativeURL);
+                profile.profileImage = imagePath;
+  
+                console.log('imatge perfil actualitzada');
+              }).catch((err) => {
+                console.log('error al descarregar imatge de perfil de cuidador ' + profile.name);
+              });
+        }
+      }
+      this.ref.detectChanges();
+    });
   }
 }
